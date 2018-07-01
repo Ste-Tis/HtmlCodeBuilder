11515e659508eddc527427c94d4b3eb154f52655
stischer <stephan.tischer@web.de>
2018-07-01 22:19:02 +0200

    Adding simple example

---
+++ example/Image.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HtmlCodeBuilder.Examples
+{
+    public static class Image
+    {
+		public static void HtmlImage()
+		{
+			var html = HtmlTag.Create("p", new HtmlElement[] {
+				HtmlText.Create("Lorem ipsum dolor sit amet, consetetur sadipscing elitr."),
+				HtmlTag.Create("figure", new[] {
+					HtmlTag.Create("img").AddAttribute("src", "https://upload.wikimedia.org/wikipedia/fr/c/c8/Assystem_Logo.jpg").AddStyles(
+						new[] { new[] {"width", "8cm"}, new[] {"height", "auto"} }
+					),
+					HtmlTag.Create("figcaption", "Assystem Logo")
+				}),
+				HtmlText.Create("Lorem ipsum dolor sit amet, consetetur sadipscing elitr.")
+			});
+
+			Console.WriteLine(html);
+		}
+    }
+}
---
+++ example/Text.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HtmlCodeBuilder.Examples
+{
+	public static class Text
+	{
+		public static void HtmlText()
+		{
+			var html = HtmlTag.Create("body", new[] {
+				HtmlTag.Create("h1", "Chapter 1"),
+				HtmlTag.Create("p", @"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod 
+									tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At 
+									vero eos et accusam et justo duo dolores et ea rebum. <b>Stet clita kasd gubergren</b>, 
+									no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit 
+									amet, <i>consetetur sadipscing elitr</i>, sed diam nonumy eirmod tempor invidunt ut 
+									labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam 
+									et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata 
+									sanctus est Lorem ipsum dolor sit amet.", false),
+				HtmlTag.Create("h2", "Chapter 1.1."),
+				HtmlTag.Create("p", @"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod 
+									tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua."),
+				HtmlTag.Create("h2", "Chapter 1.2."),
+				HtmlTag.Create("p", @"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod 
+									tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.")
+			});
+
+			Console.WriteLine(html);
+		}
+	}
+}
